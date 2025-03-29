f60f93d0ab5f41288f0f07dc1e3e191b515376b0
papucig <papuciguccici@gmail.com>
2025-03-29 20:31:14 +0000

    Add files via upload

---
+++ src/api/nitro/session/IRoomUserData.ts
@@ -0,0 +1,27 @@
+export interface IRoomUserData
+{
+    readonly roomIndex: number;
+    activityPoints: number;
+    banner: string;
+    name: string;
+    type: number;
+    sex: string;
+    figure: string;
+    custom: string;
+    webID: number;
+    groupId: number;
+    groupName: string;
+    groupStatus: number;
+    ownerId: number;
+    ownerName: string;
+    rarityLevel: number;
+    hasSaddle: boolean;
+    isRiding: boolean;
+    canBreed: boolean;
+    canHarvest: boolean;
+    canRevive: boolean;
+    hasBreedingPermission: boolean;
+    petLevel: number;
+    botSkills: number[];
+    isModerator: boolean;
+}
