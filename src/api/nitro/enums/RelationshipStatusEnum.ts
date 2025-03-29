f364e16424840b6e2a0cbc468b5ba0b6b5a1fc33
papucig <papuciguccici@gmail.com>
2025-03-29 20:29:35 +0000

    Add files via upload

---
+++ src/api/nitro/enums/RelationshipStatusEnum.ts
@@ -0,0 +1,11 @@
+export class RelationshipStatusEnum
+{
+    public static NONE: number = 0;
+    public static HEART: number = 1;
+    public static SMILE: number = 2;
+    public static BOBBA: number = 3;
+    public static EXTRA: number = 4;
+
+    public static RELATIONSHIP_TYPES = [ 0, 1, 2, 3, 4 ];
+    public static RELATIONSHIP_NAMES: string[] = ['None', 'Heart', 'Smile', 'Bobba', 'Extra'];
+}
